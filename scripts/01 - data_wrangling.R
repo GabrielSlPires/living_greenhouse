383960a24bb28f9ea9d18fac3e4322c05edc3232
GabrielSlPires <gabriel.slpires@gmail.com>
2022-07-19 14:03:12 +0200

    script to download data from google

--- scripts/01 - data_wrangling.R
+++ scripts/01 - data_wrangling.R
@@ -1,26 +1,14 @@
 library(dplyr)
-#library(googledrive)
-#
-#I need to ask for google drive file
-#drive_download("database.csv", overwrite = T)
-#id <- "1215nETQNv0kb_x-Eplk2qRTf-NE0Y6o5" # google file ID
-#pneumatron<-read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download",id),sep=",",header = F,as.is = T)
+library(googledrive)
 
+id <- "1XsTjMYXl7pNVC2qwAON51wtppDdNztZk" # google file ID
+download_link <- sprintf("https://docs.google.com/uc?id=%s&export=download", id)
 #Open pneumatron data
-# I prefer fread() because it is a bit faster than read.csv(). But further we could use a better function
-pneumatron <- data.table::fread(input = "data/database.csv",
+pneumatron <- data.table::fread(input = download_link,
                                 header = FALSE)
-
-
 
 # organize and create dataframes from Pneumatron data 
 
-#I didn't understand why you made it
-#pneumatron$error <-  substring(pneumatron$V1,1,1)
-#pneumatron$error <- as.numeric(as.character(pneumatron$error))
-#pneumatron <- na.omit(pneumatron)
-
-#I think it's easier to read the script when we break lines. but that's not a rule
 #This processes takes more than 15s
 raw <- reshape2::colsplit(string = pneumatron$V1,
                           pattern = ",",
@@ -47,7 +35,6 @@ raw <- reshape2::colsplit(string = pneumatron$V1,
                 step_min = round(time_min, 0),
                 step_min15 = plyr::round_any(step_min, 15))
 write.csv(raw, "data/pneumatron_fixed.csv", row.names = FALSE)
-
-
+message("Databe updated!")
 
 
